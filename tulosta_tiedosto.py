4d2aea381008967370df7b4689758d6560414175
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-25 22:38:05 +0200

    Korjaukset

--- tulosta_tiedosto.py
+++ tulosta_tiedosto.py
@@ -9,12 +9,16 @@ def nayta_tulokset(tiedosto):
         for j in range(len(tulokset[i])):
             if j == 0:
                 pelaaja_1 = tulokset[i][j]
+                pelaaja_1 = pelaaja_1.strip()
             if j == 1:
                 pelaaja_2 = tulokset[i][j]
+                pelaaja_2 = pelaaja_2.strip()
             if j == 2:
                 pisteet_1 = tulokset[i][j]
+                pisteet_1 = pisteet_1.strip()
             if j == 3:
                 pisteet_2 = tulokset[i][j]
+                pisteet_2 = pisteet_2.strip()
         print("{} {} - {} {}\n".format(pelaaja_1,pisteet_1,pisteet_2,pelaaja_2))
 
 nayta_tulokset("hemulicup.csv")
